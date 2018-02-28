8733a1b664baf49eb2bd168d9a37f69207af3ec0
isushkevich <ivan.sushik@gmail.com>
2018-02-28 23:04:11 +0300

    working solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports = function makeExchange( currency ) {
+  let result = {},
+    halves, quarters, dimes, nickels;
+
+  if ( !currency ) {
+    return {};
+  } else if ( currency > 10000 ) {
+    return {
+      error: "You are rich, my friend! We don't have so much coins for exchange"
+    };
+  } else { //main part
+
+    if ( currency >= 50 ) {
+      halves = Math.floor( currency / 50 );
+      result.H = halves;
+      currency = currency % 50;
+    }
+    if ( currency >= 25 ) {
+      quarters = Math.floor( currency / 25 );
+      result.Q = quarters;
+      currency = currency % 25;
+    }
+    if ( currency >= 10 ) {
+      dimes = Math.floor( currency / 10 );
+      result.D = dimes;
+      currency = currency % 10;
+    }
+    if ( currency >= 5 ) {
+      nickels = Math.floor( currency / 5 );
+      result.N = nickels;
+      currency = currency % 5;
+    }
+    if ( currency >= 1 ) {
+      result.P = currency;
+    }
+
+    return result;
+  }
 }
